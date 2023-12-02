46d61826222b2561571babc9dddb960c5e029714
W.T. Chang <1546333+wtchangdm@users.noreply.github.com>
2023-12-03 00:41:24 +0800

    refactor day 1

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,36 +1,32 @@
 fn transform_words_to_digits(line: &str) -> String {
-    let mut s = String::new();
+    line.chars()
+        .enumerate()
+        .map(|(i, _)| {
+            let letter = line.chars().nth(i).unwrap();
 
-    for (i, _) in line.chars().enumerate() {
-        let letter = line.chars().nth(i).unwrap();
-
-        match &line[i..] {
-            _ if letter.is_numeric() => s.push(letter),
-            x if x.starts_with("one") => s.push('1'),
-            x if x.starts_with("two") => s.push('2'),
-            x if x.starts_with("three") => s.push('3'),
-            x if x.starts_with("four") => s.push('4'),
-            x if x.starts_with("five") => s.push('5'),
-            x if x.starts_with("six") => s.push('6'),
-            x if x.starts_with("seven") => s.push('7'),
-            x if x.starts_with("eight") => s.push('8'),
-            x if x.starts_with("nine") => s.push('9'),
-            _ => (),
-        }
-    }
-
-    s
+            match &line[i..] {
+                _ if letter.is_numeric() => letter,
+                x if x.starts_with("one") => '1',
+                x if x.starts_with("two") => '2',
+                x if x.starts_with("three") => '3',
+                x if x.starts_with("four") => '4',
+                x if x.starts_with("five") => '5',
+                x if x.starts_with("six") => '6',
+                x if x.starts_with("seven") => '7',
+                x if x.starts_with("eight") => '8',
+                x if x.starts_with("nine") => '9',
+                _ => '\0',
+            }
+        })
+        .collect::<String>()
 }
 
 fn get_number(line: &str) -> u32 {
     let digits: Vec<char> = line.chars().filter(|c| c.is_numeric()).collect();
-    let left = digits.first().unwrap();
-    let right = digits.last().unwrap();
-    let mut s = String::new();
-    s.push(*left);
-    s.push(*right);
+    let first = digits.first().unwrap();
+    let last = digits.last().unwrap();
 
-    s.parse::<u32>().unwrap()
+    format!("{}{}", first, last).parse::<u32>().unwrap()
 }
 
 pub fn solve_part1(input: &[String]) -> u32 {
